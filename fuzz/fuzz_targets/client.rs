b4525b57c03d0fae587f4fee93556e1ab1c9b516
Mathieu Lala <m.lala@viridit.com>
2022-11-23 18:10:09 +0100

    fix: fuzzer , updated for server testing only (#756)

---
+++ fuzz/fuzz_targets/client.rs
@@ -0,0 +1,29 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    let mut command = match std::str::from_utf8(data) {
+        Ok(slice) => slice.split("\r\n"),
+        Err(_) => return,
+    };
+
+    let mut stream = std::net::TcpStream::connect("0.0.0.0:10025").unwrap();
+    let timeout = std::time::Duration::from_nanos(1);
+    stream.set_read_timeout(Some(timeout)).unwrap();
+    stream.set_write_timeout(Some(timeout)).unwrap();
+
+    let mut buffer = [0; 1024];
+    loop {
+        match std::io::Read::read(&mut stream, &mut buffer) {
+            Ok(_) => (),
+            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => return,
+            Err(e) => panic!("{:?}", e),
+        }
+
+        if let Some(i) = command.next() {
+            std::io::Write::write_all(&mut stream, i.as_bytes()).unwrap();
+        } else {
+            return;
+        }
+    }
+});
